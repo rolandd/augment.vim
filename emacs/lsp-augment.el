ef6e8a90fe278b2db7d44ad933b18a825b9fc5bb
Roland Dreier <roland@kernel.org>
2025-02-20 15:22:22 -0800

    Add rudimentary support for sending chat messages
    No support for history yet.

--- emacs/lsp-augment.el
+++ emacs/lsp-augment.el
@@ -23,9 +23,14 @@
 	(message "Already logged into Augment")
       (progn
 	(browse-url (gethash "url" signin-response))
-	(let ((auth-code (read-from-minibuffer (format "Please complete authentication in your browser...\n%s\n\nAfter authenticating, you will receive a code.\nPaste the code in the prompt below.\n\nEnter the authentication code: " (gethash "url" signin-response)))))
-	  (lsp-send-request (lsp-make-request "augment/token" (list :code auth-code)))))))
-)
+	(let ((auth-code (read-from-minibuffer (format "Please complete authentication in your browser...
+%s
+
+After authenticating, you will receive a code.
+Paste the code in the prompt below.
+
+Enter the authentication code: " (gethash "url" signin-response)))))
+	  (lsp-send-request (lsp-make-request "augment/token" (list :code auth-code))))))))
 
 (defun lsp-augment-signout ()
   "Log out of the Augment service."
@@ -33,8 +38,45 @@
   (let
       ((signout-response
 	(lsp-send-request (lsp-make-request "augment/logout" nil))))
-    (message "Signed out of Augment."))
-)
+    (message "Signed out of Augment.")))
+
+(defun lsp-augment--chat-append-text (text)
+  "Append text to the Augment chat buffer."
+  (let ((buf (get-buffer-create "*Augment Chat History*"))
+	(inhibit-read-only t))
+    (with-current-buffer buf
+      (special-mode)
+      (goto-char (point-max))
+      (insert text)
+      (pop-to-buffer buf))))
+
+(defun lsp-augment--chat-append-message (message)
+  "Append a user chat message to the Augment chat buffer."
+  (lsp-augment--chat-append-text (format "================================================================================
+
+	*You*
+
+%s
+
+--------------------------------------------------------------------------------
+
+	*Augment*
+
+" message)))
+
+(defun lsp-augment-chat (message)
+  "Send a chat request to Augment."
+  (interactive "MMessage: ")
+  (let ((chat-message (append (list :textDocumentPosition (lsp--text-document-position-params)
+				   :message message)
+			     (if (region-active-p)
+				 (list :selectedText
+				       (buffer-substring-no-properties (region-beginning) (region-end)))))))
+    (lsp-augment--chat-append-message message)
+    (lsp-request-async "augment/chat"
+		       chat-message
+		       (lambda (response) nil)
+		       :error-handler (lambda (response) (message "augment/chat returned an error")))))
 
 (defcustom lsp-augment-server-script "server.js"
   "Path to server script to run with node."
@@ -52,12 +94,18 @@
 	:vimVersion emacs-version
 	:pluginVersion "emacs 0.1.0"))
 
+(lsp-defun lsp-augment--chat-chunk-handler (_workspace params)
+  "Handler for `augment/chatChunk` notification."
+  (lsp-augment--chat-append-text (gethash "text" params)))
+
 (lsp-register-client
  (make-lsp-client
   :new-connection (lsp-stdio-connection #'lsp-augment--server-command)
   :activation-fn (lsp-activate-on "augment")
   :server-id 'augment-lsp-server
-  :initialization-options #'lsp-augment--server-initialization-options))
+  :initialization-options #'lsp-augment--server-initialization-options
+  :notification-handlers (lsp-ht
+			  ("augment/chatChunk" #'lsp-augment--chat-chunk-handler))))
 
 (provide 'lsp-augment)
 ;;; lsp-augment.el ends here
