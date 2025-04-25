a2ebf889371685480a6bb3d5a710c887579be15f
Александар Симић <alex@yaml.farm>
2025-04-25 09:12:27 +0100

    Added the missing option to pass context folders
    The Vim plugin has this option and this is an attempt to stay feature
    complete on Emacs side.
    
    It allows the user to set an option of additional directories that
    should be passed in on initialization, in order to give extra context
    to Augment Code service.

--- emacs/lsp-augment.el
+++ emacs/lsp-augment.el
@@ -26,6 +26,24 @@
   :risky t
   :type 'file)
 
+(defcustom lsp-augment-additional-context-folders nil
+  "Additional directories that Augment should index and understand.
+These directories help Augment provide better assistance by giving it
+access to related code and context. For example, if you're working on a
+module that depends on another project, you might want to add that
+project's directory here.
+
+You'd use it like so:
+
+  (setq lsp-augment-additional-context-folders
+        '(\"~/projects/shared-library\"
+          \"~/projects/core-framework\"))
+
+Note: you'd remove those backslashes before the double quotes in your
+configuration."
+  :group 'lsp-augment
+  :type '(repeat directory))
+
 (defun lsp-augment-signin ()
   "Log into the Augment service."
   (interactive)
@@ -166,6 +184,14 @@ Returns a plist with status information from the server."
 	:vimVersion emacs-version
 	:pluginVersion "emacs 0.1.0"))
 
+(defun lsp-augment--get-workspace-folders ()
+  "Convert workspace folders to LSP format."
+  (when lsp-augment-additional-context-folders
+    (mapcar (lambda (folder)
+              (list :uri (lsp--path-to-uri folder)
+                    :name (file-name-nondirectory (directory-file-name folder))))
+            lsp-augment-additional-context-folders)))
+
 (lsp-register-client
  (make-lsp-client
   :new-connection (lsp-stdio-connection #'lsp-augment--server-command)
@@ -175,6 +201,7 @@ Returns a plist with status information from the server."
   :add-on? t
   :completion-in-comments? t
   :initialization-options #'lsp-augment--server-initialization-options
+  :workspace-folders #'lsp-augment--get-workspace-folders
   :notification-handlers (lsp-ht
 			  ("augment/chatChunk" #'lsp-augment--chat-chunk-handler))))
 
